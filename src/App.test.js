7468fa3b630a49102063c20fc630bbe6b1a75782
Fifolu Akinyemi <fkinyemi@gmail.com>
2021-09-08 14:48:07 +0100

    Completed first feature test. It tests for a drop down list of cat breeds

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,22 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// fetch all cat breeds from api, test react app displays all cat breeds in a drop down list
+
+const catApi = 
+  await fetch('https://api.thecatapi.com/v1/breeds')
+  .then(res => res.json())
+  .then(data => data)
+;
+
+it('should display a drop down list of cat breeds', () => {
+  render(<AllBreeds />);
+
+  expect(screen.getByLabelText(/Select a breed/i)).toBeInTheDocument();
+  expect(screen.getByTestId(/breed selection/i)).toEqual('Abyssinian');
+
+  userEvent.change(screen.getByTestId(/breed selection/i), {
+    target: { value: "Donskoy" }
+  })
+  expect(screen.getByText("Origin: Russia"));
 });
