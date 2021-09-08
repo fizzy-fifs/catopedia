0e5b23da1de2f539f7a4083b83a2e8697da4449f
Fifolu Akinyemi <fkinyemi@gmail.com>
2021-09-08 22:09:26 +0100

    Created test for rendering breed info

--- src/App.test.js
+++ src/App.test.js
@@ -2,27 +2,41 @@ import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import App from "./App";
 import AllBreeds from "./components/AllBreeds";
+import SelectABreed from "./components/SelectABreed";
 
 // fetch all cat breeds from api, test react app displays all cat breeds in a drop down list
 
 
 it("should display a drop down list of cat breeds", async() => {
-  render(<AllBreeds />);
+  render(<SelectABreed />);
 
   expect(screen.getByText("Select A Breed")).toBeInTheDocument();
  
   await waitFor(() => {
     let breeds = screen.getByTestId("breed selection");
-    
     let selectedOption = userEvent.selectOptions(screen.getByTestId("breed selection"), "Aegean")
-   
-    expect(screen.getByText("Aegean")).toBeInTheDocument;
-  })
+  }) 
+  
+  expect(screen.getByText("Aegean")).toBeInTheDocument;
+  
 });
 
 
 // Render all breed info eg.: name, country of origin etc
 
+it("should render breed info", async() => {
+  render(<AllBreeds />);
 
+  expect(screen.getByText("Select A Breed")).toBeInTheDocument();
+
+  await waitFor(() => {
+    let breeds = screen.getByTestId("breed selection");
+    let selectedOption = userEvent.selectOptions(screen.getByTestId("breed selection"), "Abyssinian")
+  })
+
+  expect(screen.getByText("Abyssinian")).toBeInTheDocument;
+  expect(screen.getByText("Description")).toBeInTheDocument;
+  expect(screen.getByRole(Image)).toBeInTheDocument;
+})
 
 //css
